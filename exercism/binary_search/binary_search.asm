32130ae5a2fb4db790a347cc8b2aaf28fc6f44e3
nikaroxanne <nikakw@gmail.com>
2022-07-21 21:10:03 -0400

    adding version 9 of binary_search.asm solution, working solution, passes all tests, cleaned up code/comments

--- exercism/binary_search/binary_search.asm
+++ exercism/binary_search/binary_search.asm
@@ -17,6 +17,13 @@ find:
     je binary_search_empty
     ;; args will get pushed in reverse order
     ;; so push in reverse order of (array, sizeof(array), key) = (rdi, rsi, rdx)
+    ;; push rbp, mov rbp to rsp
+    ;; then push args rdi, rsi, rdx in reverse order
+    ;; finally subtract size of stack frame from rsp (8 * 3 = 24), so rsp=rsp-24
+    ;; size of stack frame is 24 because we are pushing 3 8-byte registers onto the stack
+    ;; could we make do with a smaller stack frame, assuming 32bit integer values? 
+    ;; yeah probably, but then I would have to rewrite a good portion of this to specify that it's 32bit
+    ;; also the problem spec didn't specify word size of int values, so I'm **playing it safe** lmao w 64bit
     push rbp
     mov rsp, rbp
     push rdx
@@ -41,15 +48,17 @@ binary_search_non_empty:
   ;; index of key
   xor eax, eax
   
-  ;; high
-  ;; low
+  ;; ebx = low
   xor ebx, ebx
   mov dword[rbp-12], ebx
   mov ebx, dword[rbp-12]
  
-  ;;so [rbp-8*i] where i is some integer, are addresses of local variables within the function
-  ;; so we can use [rbp -8] and [rbp - 16] etc etc, to store the values of our variables low, high and mid, rather than try to use the intermediaries of the registers, only some which are callee-save registers and won't preserve their values between diff calls i.e. within the loop body etc. (yeah that's a long-winded explanation of what a local variable is; I don't care. I find tangential explanations in code comments useful. Also it's my code, my rules. Why are you even in this comment in the first place? This is for my own notes. ugh!!)
+  ;;  so [rbp-8*i], where i is some integer, are addresses of local variables within the function
+  ;; so we can use [rbp -8] and [rbp - 16] etc etc, to store the values of our variables low, high and mid, rather than try to use the intermediaries of the registers, 
+  ;; only some of which are callee-save registers and won't preserve their values between diff calls i.e. within the loop body etc. 
   
+  
+  ;; ecx = high
   ;;set ecx to value of ARRAY_SIZE, which is on the stack at rbp+16
   mov ecx, dword[rbp+16]
   mov dword[rbp-4], ecx
@@ -57,16 +66,16 @@ binary_search_non_empty:
   dec dword[rbp-4]
   mov ecx, dword[rbp-4]
   
-;;if (low > high), then array is invalid, break out of this function, return -1
   jmp bin_search_loop 
   
 bin_search_loop:
   cmp ebx, dword[rbp-4]
   jnl binary_search_empty
+  ;;  if (low > high), then array is invalid, break out of this function, return -1
 
   ;;mid = (low + high) // 2
-  ;;eax:edx / ecx
-  ;; div ecx (result goes in ecx) 
+  ;; 
+  ;; 
   ;;
   mov r12d, dword[rbp-8]
   add r12d, dword[rbp-12]
@@ -98,12 +107,13 @@ left_recurse:
   dec dword[rbp-4]
   mov ecx, dword[rbp-4]
   jmp bin_search_loop
+  ;;
   ;; originally, when initializing value of high:
-  ;;mov cx, word[rbp+16]
-  ;;mov word[rbp-4], cx
+  ;; mov ecx, dword[rbp+16]
+  ;; mov word[rbp-4], cx
   ;; Now:
   ;; high = mid -1
-  ;; cx = word[rbp-8]
+  ;; ecx = dword[rbp-8]
   ;;
     
 right_recurse:
@@ -111,11 +121,12 @@ right_recurse:
   inc dword[rbp-12]
   mov ebx, dword[rbp-12]
   jmp bin_search_loop
+  ;;
   ;; originally, when initializing value of low:
-  ;;mov word[rsp-12], bx
-  ;;mov bx, word[rsp-12]
+  ;; mov dword[rsp-12], ebx
+  ;; mov ebx, word[rsp-12]
   ;; Now:
-  ;;low = mid + 1
-  ;; rbx = word[rsp - 12] 
+  ;; low = mid + 1
+  ;; ebx = dword[rsp - 12] 
 
 
